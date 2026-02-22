5c7f992e5d1ea43641f0757d43009c047ac8d1b0
Maysun Dietrick <dietrickm16@students.ecu.edu>
2026-02-21 20:07:32 -0500

    Load JSON + render component

--- web/react-app/src/App.tsx
+++ web/react-app/src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import sample from "./sample-question.json";
+import { QuestionRenderer } from "./components/QuestionRenderer";
+import { QuestionDefinition } from "./types/QuestionDefinition";
 
 function App() {
+  const [question, setQuestion] = useState<QuestionDefinition | null>(null);
+
+  useEffect(() => {
+    setQuestion(sample);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React Dynamic Question</h1>
+      {question && <QuestionRenderer question={question} />}
     </div>
   );
 }
