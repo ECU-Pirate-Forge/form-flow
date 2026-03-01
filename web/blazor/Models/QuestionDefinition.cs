b72b87cd4a2ca1c1ecae8f1f382ca17512f79f99
Maysun Dietrick <dietrickm16@students.ecu.edu>
2026-03-01 10:16:55 -0500

    added placeholder, required, defaultvalue, and helptext to blazor template

--- web/blazor/Models/QuestionDefinition.cs
+++ web/blazor/Models/QuestionDefinition.cs
@@ -8,5 +8,10 @@ namespace FormFlow.Blazor.Models
         public string? Id { get; set; }
         public string? Label { get; set; }
         public string? Type { get; set; }
+
+        public string? Placeholder {get; set; }
+        public bool? Required { get; set; }
+        public string? DefaultValue { get; set; }
+        public string? HelpText { get; set; }
     }
 }
