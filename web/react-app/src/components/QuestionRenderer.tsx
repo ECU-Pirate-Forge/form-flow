37c5bc4ea9065ea630d85fba4de531409407f2e2
Maysun Dietrick <dietrickm16@students.ecu.edu>
2026-03-01 10:27:30 -0500

    added styling to react template

--- web/react-app/src/components/QuestionRenderer.tsx
+++ web/react-app/src/components/QuestionRenderer.tsx
@@ -7,18 +7,29 @@ interface Props {
 
 export const QuestionRenderer: React.FC<Props> = ({ question }) => {
     return (
-        <div>
-            <label>{question.label}</label>
+        <div style={{ maxWidth: "400px", marginBottom: "1rem"}}>
+            <label style={{ display: "block", fontWeight: 600, marginBottom: "4px"}}>
+                {question.label}
+                {question.required && (
+                    <span style={{ color: "red", marginLeft: "4px"}}>*</span>
+                )}
+                </label>
 
             <input 
                 type="text"
                 placeholder={question.placeholder}
                 required={question.required}
                 defaultValue={question.defaultValue}
+                style={{
+                    padding: "8px",
+                    borderRadius: "4px",
+                    border: "1px solid #ccc",
+                    width: "100%"
+                }}
             />
 
             {question.helpText && (
-                <small style={{ display: "block", marginTop: "4px"}}>
+                <small style={{ display: "block", marginTop: "4px", color: '#666'}}>
                     {question.helpText}
                 </small>
             )}        
