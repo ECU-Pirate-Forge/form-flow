dabe2052dc08483f4872a58258b47c09d22bdd92
Maysun Dietrick <dietrickm16@students.ecu.edu>
2026-03-14 23:03:40 -0400

    added get endpoint

---
+++ FormFlow.Backend/Endpoints/QuestionEndpoints.cs
@@ -0,0 +1,23 @@
+using FormFlow.Data.Models;
+using FormFlow.Backend.Templates;
+
+namespace FormFlow.Backend.Endpoints
+{
+    public static class QuestionEndpoints
+    {
+        public static void MapQuestionEndpoints(this IEndpointRouteBuilder app)
+        {
+            app.MapGet("/api/questions/template", () =>
+            {
+                var question = SingleQuestionTemplate.Get();
+                return Results.Json(question);
+            })
+            .WithName("GetQuestionTemplate")
+            .Produces<Question>(StatusCodes.Status200OK)
+            .Produces(StatusCodes.Status400BadRequest)
+            .Produces(StatusCodes.Status404NotFound)
+            .Produces(StatusCodes.Status500InternalServerError)
+            .Produces(StatusCodes.Status503ServiceUnavailable);
+        }
+    }
+}
