015d57d3688e984967301271c2441a3bc54df205
Maysun Dietrick <dietrickm16@students.ecu.edu>
2026-03-26 10:28:41 -0400

    loads question, stores in state, and UI stores number of questions loaded

--- FormFlow.React/src/App.tsx
+++ FormFlow.React/src/App.tsx
@@ -1,19 +1,18 @@
 import React, { useEffect, useState } from "react";
-import sample from "./sample-question.json";
-import { QuestionRenderer } from "./components/QuestionRenderer";
+import sample from "./multiple-sample-questions.json";
 import { QuestionDefinition } from "./types/QuestionDefinition";
 
 function App() {
-  const [question, setQuestion] = useState<QuestionDefinition | null>(null);
+  const [questions, setQuestions] = useState<QuestionDefinition[]>([]);
 
   useEffect(() => {
-    setQuestion(sample);
+    setQuestions(sample);
   }, []);
 
   return (
     <div>
       <h1>React Dynamic Question</h1>
-      {question && <QuestionRenderer question={question} />}
+      <p>Loaded {questions.length} questions.</p>
     </div>
   );
 }
