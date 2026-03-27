35a9f42b6f08e472b358bb96b5a88c5280525457
Jared Rogers <jareleerogers@gmail.com>
2026-03-27 17:35:53 -0400

    feat(react): support independent QuestionRenderer instances in loops

--- FormFlow.React/src/components/QuestionRenderer.tsx
+++ FormFlow.React/src/components/QuestionRenderer.tsx
@@ -1,47 +1,60 @@
 import React from "react";
 import { QuestionDefinition } from "../types/QuestionDefinition";
 
-interface Props {
+export interface QuestionRendererProps {
     question: QuestionDefinition;
 }
 
-export const QuestionRenderer: React.FC<Props> = ({ question }) => {
-    const inputId = `question-${question.id}-${question.key}`;
-    const helpTextId = question.helpText ? `${inputId}-help` : undefined;
+export class QuestionRenderer extends React.PureComponent<QuestionRendererProps> {
+    private static nextInstanceNumber = 0;
 
-    return (
-        <div style={{ 
-            maxWidth: "400px", 
-            marginBottom: "1rem",
-            padding: "16px",
-          }}>
-            <label htmlFor={inputId} style={{ display: "block", fontWeight: 600, marginBottom: "4px"}}>
-                {question.label}
-                {question.required && (
-                    <span style={{ color: "red", marginLeft: "4px"}}>*</span>
-                )}
+    private readonly instanceId: number;
+
+    public constructor(props: QuestionRendererProps) {
+        super(props);
+        QuestionRenderer.nextInstanceNumber += 1;
+        this.instanceId = QuestionRenderer.nextInstanceNumber;
+    }
+
+    public render(): React.ReactNode {
+        const { question } = this.props;
+        const inputId = `question-${question.id}-${question.key}-${this.instanceId}`;
+        const helpTextId = question.helpText ? `${inputId}-help` : undefined;
+
+        return (
+            <div style={{
+                maxWidth: "400px",
+                marginBottom: "1rem",
+                padding: "16px",
+            }}>
+                <label htmlFor={inputId} style={{ display: "block", fontWeight: 600, marginBottom: "4px" }}>
+                    {question.label}
+                    {question.required && (
+                        <span style={{ color: "red", marginLeft: "4px" }}>*</span>
+                    )}
                 </label>
 
-            <input 
-                id={inputId}
-                type="text"
-                placeholder={question.placeholder}
-                required={question.required}
-                defaultValue={question.defaultValue ?? undefined}
-                aria-describedby={helpTextId}
-                style={{
-                    padding: "8px",
-                    borderRadius: "4px",
-                    border: "1px solid #ccc",
-                    width: "100%"
-                }}
-            />
+                <input
+                    id={inputId}
+                    type="text"
+                    placeholder={question.placeholder}
+                    required={question.required}
+                    defaultValue={question.defaultValue ?? undefined}
+                    aria-describedby={helpTextId}
+                    style={{
+                        padding: "8px",
+                        borderRadius: "4px",
+                        border: "1px solid #ccc",
+                        width: "100%"
+                    }}
+                />
 
-            {question.helpText && (
-                <small id={helpTextId} style={{ display: "block", marginTop: "4px", color: '#666'}}>
-                    {question.helpText}
-                </small>
-            )}        
-        </div>
-    );
-};
+                {question.helpText && (
+                    <small id={helpTextId} style={{ display: "block", marginTop: "4px", color: "#666" }}>
+                        {question.helpText}
+                    </small>
+                )}
+            </div>
+        );
+    }
+}
