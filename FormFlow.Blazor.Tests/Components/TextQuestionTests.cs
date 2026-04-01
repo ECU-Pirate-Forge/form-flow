62520a711a8fb048a0186fbe7da00d9820020348
Brayan Jimeno <jimenob21@students.ecu.edu>
2026-04-01 15:58:17 +0000

    tests and removed shared import from home

--- FormFlow.Blazor.Tests/Components/TextQuestionTests.cs
+++ FormFlow.Blazor.Tests/Components/TextQuestionTests.cs
@@ -1,33 +1,86 @@
 using Bunit;
 using Xunit;
-using FormFlow.Blazor.Components.Shared;
+using FluentAssertions;
+using MudBlazor;
+using MudBlazor.Services;
+using FormFlow.Blazor.Components.QuestionTypes;
+using FormFlow.Data.Models;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace FormFlow.Blazor.Tests.Components;
-public class TextQuestionTests : BunitContext {
+
+public class TextQuestionTests : BunitContext
+{
+
+    private void SetupServices()
+    {
+        JSInterop.Mode = JSRuntimeMode.Loose;
+        Services.AddMudServices();
+        Services.AddSingleton(TimeProvider.System);
+
+        // Render MudBlazor providers directly into the current context
+        Render<MudThemeProvider>();
+        Render<MudPopoverProvider>();
+        Render<MudDialogProvider>();
+        Render<MudSnackbarProvider>();
+    }
     [Fact]
-        public void TextQuestion_ShouldRenderCorrectLabelAndInput()
+    public async Task Should_Render_Label_And_HelpText_WhenProvided()
+    {
+        SetupServices();
+        var question = CreateTextQuestion("");
+        
+        var cut = Render<TextQuestion>(p => p.Add(x => x.Question, question));
+
+        cut.Markup.Should().Contain("Favorite Programmming Language");
+        cut.Markup.Should().Contain("Python, C#, JavaScript, etc.");
+
+        await DisposeAsync();
+        
+    }
+
+    [Fact]
+    public async Task Should_Render_Asterisk_When_Required()
+    {
+        SetupServices();
+        var question = CreateTextQuestion("");
+        question.Required = true;
+        
+
+        var cut = Render<TextQuestion>(p => p.Add(x => x.Question, question));
+
+        cut.Markup.Should().Contain("Favorite Programmming Language");
+        cut.Markup.Should().Contain("*");
+
+        await DisposeAsync();
+    }
+
+    [Fact]
+    public async Task Should_UpdateValue_When_Input_Changes()
+    {
+        SetupServices();
+        var question = CreateTextQuestion("");
+        
+        var cut = Render<TextQuestion>(p => p.Add(x => x.Question, question));
+
+        var input = cut.Find("input");
+        input.Change("C#");
+
+        input.GetAttribute("value").Should().Be("C#");
+        await DisposeAsync();
+    }
+
+    private static QuestionDefinition CreateTextQuestion(string? defaultValue)
+    {
+        return new QuestionDefinition
         {
-            // 1. Arrange: Define the parameters we want to pass to the component
-            string expectedLabel = "What is your name?";
-            string expectedValue = "John Doe";
-
-            // 2. Act: Render the component
-            var cut = Render<TextQuestion>(parameters => parameters
-                .Add(p => p.Label, expectedLabel)
-                .Add(p => p.Value, expectedValue)
-            );
-
-            // 3. Assert: Verify "Label is displayed"
-            // This looks for a <label> tag and checks if the text matches
-            var labelHtml = cut.Find("label");
-            labelHtml.MarkupMatches($"<label>{expectedLabel}</label>");
-
-            // 4. Assert: Verify "<input type='text'> exists"
-            // This finds the input and checks its attributes
-            var inputHtml = cut.Find("input");
-            Assert.Equal("text", inputHtml.GetAttribute("type"));
-
-        // 5. Assert: Verify the value is correctly bound
-        Assert.Equal(expectedValue, inputHtml.GetAttribute("value"));
-        }
+            Id = Guid.NewGuid(),
+            Key = "favorite_language",
+            Label = "Favorite Programmming Language",
+            Type = "text",
+            DefaultValue = defaultValue,
+            HelpText = "Python, C#, JavaScript, etc.",
+            
+        };
+    }
 }
