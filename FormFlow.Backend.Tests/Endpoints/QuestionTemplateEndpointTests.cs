3ba5311c0c5250f9743a4a7fc8d85588748499e9
Brayan Jimeno <jimenob21@students.ecu.edu>
2026-04-16 21:37:46 +0000

    made test use temp db

--- FormFlow.Backend.Tests/Endpoints/QuestionTemplateEndpointTests.cs
+++ FormFlow.Backend.Tests/Endpoints/QuestionTemplateEndpointTests.cs
@@ -1,14 +1,38 @@
 using System.Net;
 using System.Net.Http.Json;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.Hosting;
 using Xunit;
 using FormFlow.Data.Models;
+using Microsoft.Extensions.DependencyInjection;
+using LiteDB;
 
 
 namespace FormFlow.Backend.Tests.Endpoints
 {
     public class BackendFactory : WebApplicationFactory<Program>
     {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                // Remove the default LiteDB registration
+                var descriptor = services.FirstOrDefault(
+                    d => d.ServiceType == typeof(ILiteDatabase));
+
+                if (descriptor != null)
+                {
+                    services.Remove(descriptor);
+                }
+
+                // Use an in-memory database for testing
+                services.AddSingleton<ILiteDatabase>(sp =>
+                {
+                    var dbPath = Path.Combine(Path.GetTempPath(), $"formflow-test-{Guid.NewGuid():N}.db");
+                    return new LiteDatabase($"Filename={dbPath};Connection=shared");
+                });
+            });
+        }
     }
 
     public class QuestionTemplateEndpointTests : IClassFixture<BackendFactory>
