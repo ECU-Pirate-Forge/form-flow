b21e2ab20c18479f674a7d5bae7daeba2b1d255d
Brayan Jimeno <jimenob21@students.ecu.edu>
2026-04-19 04:26:59 +0000

    testing fixes

--- FormFlow.Backend.Tests/Endpoints/QuestionPostEndpointTest.cs
+++ FormFlow.Backend.Tests/Endpoints/QuestionPostEndpointTest.cs
@@ -1,18 +1,50 @@
 using System.Net;
 using System.Net.Http.Json;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Xunit;
 using FormFlow.Data.Models;
 using FluentAssertions;
-
+using Microsoft.AspNetCore.Hosting;
+using LiteDB;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace FormFlow.Backend.Tests.Endpoints
 {
-    public class QuestionPostEndpointTests : IClassFixture<BackendFactory>
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>, IDisposable
+    {
+        public string tempDbPath { get; } = Path.Combine(
+            Path.GetTempPath(),
+            $"formflow-endpoint-tests-{Guid.NewGuid():N}.db");
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var existingDatabaseRegistration = services.FirstOrDefault(d => d.ServiceType == typeof(ILiteDatabase));
+                if (existingDatabaseRegistration is not null)
+                {
+                    services.Remove(existingDatabaseRegistration);
+                }
+                
+                services.AddSingleton<ILiteDatabase>(_ =>
+                    new LiteDatabase($"Filename={tempDbPath};Connection=shared"));
+            });
+        }
+        public new void Dispose()
+        {
+            base.Dispose();
+
+            if (File.Exists(tempDbPath))
+            {
+                File.Delete(tempDbPath);
+            }
+        }
+    }
+
+    
+    public class QuestionPostEndpointTests : IClassFixture<CustomWebApplicationFactory>
     {
         private readonly HttpClient _client;
 
-        public QuestionPostEndpointTests(BackendFactory factory)
+        public QuestionPostEndpointTests(CustomWebApplicationFactory factory)
         {
             _client = factory.CreateClient();
         }
@@ -21,10 +53,11 @@ namespace FormFlow.Backend.Tests.Endpoints
         public async Task Post_ValidQuestion_Returns201Created()
         {
             // Arrange
+            var uniqueKey = $"email-{Guid.NewGuid():N}";
             var validQuestion = new QuestionDefinition
             {
-                Id = Guid.NewGuid(),
-                Key = "email",
+                //Id = Guid.NewGuid(),
+                Key = uniqueKey,
                 Label = "Email Address",
                 Type = "text",
                 Required = true
@@ -38,7 +71,7 @@ namespace FormFlow.Backend.Tests.Endpoints
 
             var createdQuestion = await response.Content.ReadFromJsonAsync<QuestionDefinition>();
             createdQuestion.Should().NotBeNull();
-            createdQuestion!.Key.Should().Be("email");
+            createdQuestion!.Key.Should().Be(uniqueKey);
             createdQuestion.Label.Should().Be("Email Address");
             createdQuestion.Type.Should().Be("text");
             createdQuestion.Id.Should().NotBe(Guid.Empty);
