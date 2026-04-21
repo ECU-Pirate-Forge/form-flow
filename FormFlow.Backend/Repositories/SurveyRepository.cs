070496b34f4b92a42fa985540dcbc92a20d172cf
Maysun Dietrick <dietrickm16@students.ecu.edu>
2026-04-21 18:19:29 -0400

    created survey repository

---
+++ FormFlow.Backend/Repositories/SurveyRepository.cs
@@ -0,0 +1,22 @@
+using LiteDB;
+using FormFlow.Data.Models;
+
+namespace FormFlow.Backend.Repositories
+{
+    public class SurveyRepository : ISurveyRepository
+    {
+        public ILiteCollection<SurveyDefinition> Surveys { get; }
+
+        public SurveyRepository(ILiteDatabase db)
+        {
+            Surveys = db.GetCollection<SurveyDefinition>("surveys");
+
+            Surveys.EnsureIndex(s => s.Id);
+        }
+    }
+
+    public interface ISurveyRepository
+    {
+        ILiteCollection<SurveyDefinition> Surveys { get; }
+    }
+}
