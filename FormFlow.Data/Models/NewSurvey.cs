b78d7d545129116da57aa11c94357cd786d634e8
Maysun Dietrick <dietrickm16@students.ecu.edu>
2026-04-23 23:30:11 -0400

    updated new survey model to match input received from the UI

--- FormFlow.Data/Models/NewSurvey.cs
+++ FormFlow.Data/Models/NewSurvey.cs
@@ -4,14 +4,9 @@ namespace FormFlow.Data.Models
 {
     public class NewSurvey
     {
-        [BsonId]
-        public Guid Id { get; set; }
-
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public List<Guid> QuestionIds { get; set; } = [];
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-
-        public int? Version { get; set; }
+        //public int? Version { get; set; }
     }
 }
